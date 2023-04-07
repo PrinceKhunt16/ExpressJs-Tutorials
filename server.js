5478b8086ec8728a6d614de90237428e7a18148e
Prince <princekhuntdatastructure@gmail.com>
2023-04-07 15:15:09 +0530

    Integreat ejs

--- server.js
+++ server.js
@@ -1,8 +1,22 @@
 const express = require('express')
+const path = require('path')
 const app = express()
 
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+
 app.get('/', (req, res) => {
-    res.send('Hello from express')
+    // res.sendFile(path.resolve(__dirname) + '/index.html')
+    res.render('index', {
+        title: 'Homepage'
+    })
+})
+
+app.get('/about', (req, res) => {
+    // res.sendFile(path.resolve(__dirname) + '/about.html')
+    res.render('about', {
+        title: 'Aboutpage'
+    })
 })
 
 app.listen(3000, () => {
